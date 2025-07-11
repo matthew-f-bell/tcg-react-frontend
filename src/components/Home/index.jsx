4c631fdb25399b173858a8b0e4e87af924b4d8fb
matthew-f-bell <xdjx3runx@gmail.com>
2025-07-11 15:15:53 -0700

    cards and grid setup for use

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,9 +1,53 @@
 import React from 'react';
+import Tcgcard from '../TcgCard';
+import { useState } from 'react';
+
+
+function createInitialCards() {
+  const initialCardDisplay = [];
+  for (let i = 0; i < 5; i++) {
+    initialCardDisplay.push({
+      id: i,
+      card: <Tcgcard />
+    });
+  }
+  return initialCardDisplay;
+}
 
 // Home Page Component
 function Home() {
+  const [cards, setCards] = useState(createInitialCards());
+
   return(
-    <h1>Test</h1>
+    <>
+      <h1>Test</h1>
+      <button onClick={() => {
+        setCards([
+          {
+            id: cards.length,
+            card: <Tcgcard />
+          }, ...cards]);
+      }}>
+        Increment Component  
+      </button>
+      <button onClick={() => {
+        setCards([
+          {
+            id: cards.length,
+            card: <Tcgcard />
+          }, ...cards]);
+      }}>        Decrement Component
+      </button>
+      <div class="card-container">
+        {
+          cards.map(item => (
+            <div key={item.id}>
+              {<Tcgcard />}
+            </div>
+          ))
+        }
+      </div>
+    </>
   );
 }
 
--- src/components/TcgCard/index.jsx
+++ src/components/TcgCard/index.jsx
@@ -5,12 +5,14 @@ import '../../stylesheets/index.scss';
 // Component for Cards
 function Tcgcard () {
     return (
-        <div class="tcg-card">
-            <img src="/assets/staticImages/placeholder_img.png" alt="" />
-            <div>
-                Placeholder Text
+        <>
+            <div class="tcg-card">
+                <img class="card-img" src="/assets/staticImages/placeholder_img.png" alt="" />
+                <div>
+                    Placeholder Text
+                </div>
             </div>
-        </div>
+        </>
     );
 }
 
